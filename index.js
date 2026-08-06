31ef0623e703e42fcfc1b6f37057efb66b101e8a
slim-77 <mne63001@gmail.com>
2026-08-07 01:44:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '967780899888', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
